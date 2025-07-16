d6a9aaec56750572897cfa513f9620921a74f243
Lucas <santoslucascosme12@gmail.com>
2025-07-16 19:51:13 -0300

    feat: criando projeto e entidade produto

---
+++ Domain/Entites/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Deposito.Domain.Entites
+{
+    public class Product
+    {
+        public Guid Id { get; set;}
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
