2ed895d176871b3798a00ceb0c4c0a7ab06d602f
Lucas <santoslucascosme12@gmail.com>
2025-09-30 15:19:40 -0300

    feat: criando entitade pedido

---
+++ Domain/Entites/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Deposito.Domain.Entites
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string Addres { get; set; } = string.Empty;
+        public bool InStorePickup { get; set; } 
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public Product ProductId { get; set; } 
+        public Client ClientId { get; set; }
+    }
+}
